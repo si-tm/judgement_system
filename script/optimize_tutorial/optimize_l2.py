18d7d28ec36db57572f6e8aa673a13103272cbf9
Si-tm <57259352+si-tm@users.noreply.github.com>
2023-07-20 10:01:25 +0900

    Update optimize_l2.py steps 1000000

--- script/optimize_tutorial/optimize_l2.py
+++ script/optimize_tutorial/optimize_l2.py
@@ -68,7 +68,7 @@ def run_qdpy(dirpath="test"):
     #??
     algo = algorithms.RandomSearchMutPolyBounded(
         grid, 
-        budget=10000, 
+        budget=1000000, 
         # budget=1000, 
         batch_size=100,
         # dimension=17, #1つのストランドセットに幾つパラメータがあるか # one bit per strand
